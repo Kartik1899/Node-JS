7d71946a68ad2206e5e5be1d7328bcd79a9c8766
kartik1899 <ranakartik16@gmail.com>
2019-10-01 11:09:26 +0530

    Arrays in js

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+var a = [10,12,13];                     // Declaring array in js
+
+a.push(14);                             // Inserts value at front
+console.log(a);
+
+a.pop();                                // Removes element from the end
+console.log(a);
+
+a.shift();                              // Removes element from front
+console.log(a);
+
+a.unshift(5);                           // Inserts element at front
+console.log(a);
